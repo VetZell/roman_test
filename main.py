474d6e26f1b5cd821eb27a8fcbff7374da9859d4
VetZell <killrot6666@gmail.com>
2026-07-21 09:48:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+
+from fastapi.responses import FileResponse
+
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+
+async def root():
+
+    return FileResponse("static/index.html")
 
 
 @app.get("/", response_class=HTMLResponse)
